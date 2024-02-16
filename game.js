ee7857345bfd890ad354f436a57f570170314026
gh-charles <blakos244@gmail.com>
2024-02-15 18:18:20 -0600

    final fixes

--- game.js
+++ game.js
@@ -1,14 +1,15 @@
 import { parse } from './node_modules/@vanillaes/csv/index.js'
 
-var QUESTIONS_ANSWERS = `calebcity/comedy,geography,anime openings/endings,easy,media mix
-img/C:/Users/HP/OneDrive/SAND/game/assets/caleb1.JPG:This is how it feels sometimes,What is the very large desert in North Africa?:Sahara,audio/C:/Users/HP/OneDrive/SAND/game/assets/Attack on Titan Season 2 - Opening _ Shinzou wo Sasageyo! (1080p_24fps_H264-128kbit_AAC).mp4:Attack on Titan OP 2,How many times can you subtract 10 from 100?:Once. Next time you would be subtracting 10 from 90.,audio/C:/Users/HP/OneDrive/SAND/game/assets/One Piece _ Opening 9 - Jungle P _ 4K Creditless (2160p_24fps_VP9 LQ-160kbit_Opus).webm:One Piece - Opening 9
-img/C:/Users/HP/OneDrive/SAND/game/assets/caleb2.JPG:Me shopping for anything.,img/C:/Users/HP/OneDrive/SAND/game/assets/Vector-Italy-Map-PNG-Free-Download-1799540212.png:Italy,audio/C:/Users/HP/OneDrive/SAND/game/assets/ENDING 1 _ BLEACH _ Life is Like a Boat by Rie fu _ VIZ (1080p_30fps_H264-128kbit_AAC).mp4:ENDING 1 BLEACH,"Before Mount Everest was discovered, what was the tallest mountain on Earth?:everest",audio/C:/Users/HP/OneDrive/SAND/game/assets/Neon Genesis Evangelion - Opening (Creditless) (HD - 60 fps) (1440p_60fps_VP9-160kbit_Opus).webm:Neon Genesis Evangelion
-img/C:/Users/HP/OneDrive/SAND/game/assets/caleb3.JPG:Basically me for a month,img/C:/Users/HP/OneDrive/SAND/game/assets/WRLD-CAM-01-0001-1876659496.png:Central America,audio/C:/Users/HP/OneDrive/SAND/game/assets/My Hero Academia - Opening 2 _ Peace Sign (1080p_24fps_H264-128kbit_AAC).mp4:My Hero Academia - Opening 2,"If the vice president were to die, who is supposed to be president?:President",audio/C:/Users/HP/OneDrive/SAND/game/assets/Fairy Tail - Opening 3 [Official Lyrics Video] [HD_HQ] (1080p_30fps_H264-128kbit_AAC).mp4:Fairy Tail - Opening 3
-img/C:/Users/HP/OneDrive/SAND/game/assets/caleb4.JPG:The friend thats always forgetting something,What is the large ethnic group that migrated to the Sub-Saharah many years ago?:Bantu,audio/C:/Users/HP/OneDrive/SAND/game/assets/Tokyo Ghoul - Opening _ Munou (1080p_24fps_H264-128kbit_AAC).mp4:Tokyo Ghoul - Opening 2,img/C:/Users/HP/OneDrive/SAND/game/assets/shroud-1.avif:The Shroud,audio/C:/Users/HP/OneDrive/SAND/game/assets/Banjo-Kazooie - trailer (240p_30fps_H264-96kbit_AAC).mp4:Banjo-Kazooie
-img/C:/Users/HP/OneDrive/SAND/game/assets/kp.JPG:Killing an African Warlord,Name of warrior ethnic group in south africa:Zulu,audio/C:/Users/HP/OneDrive/SAND/game/assets/Re_ZERO -Starting Life in Another World- Season 2 - Opening _ Realize (1080p_24fps_H264-128kbit_AAC).mp4:ReZero season 2 op 1,img/C:/Users/HP/OneDrive/SAND/game/assets/22-1-88932007.jpg:Fighting American,audio/C:/Users/HP/OneDrive/SAND/game/assets/maxresdefault-2710931489.jpg:Penguins of Madagascar
-img/C:/Users/HP/OneDrive/SAND/game/assets/desmond.JPG:I hate sleep paralysis the MOST.,"What is notable about the walls of benin? (west africa):Largest collective structure in the world, longer than Great walls of China",audio/C:/Users/HP/OneDrive/SAND/game/assets/Nichijou My Ordinary Life - Opening _ Hyadain no Kakakata☆Kataomoi - C (1080p_24fps_H264-128kbit_AAC).mp4:Nichijou op 1,img/C:/Users/HP/OneDrive/SAND/game/assets/8q69kbpaj09x-2804535359.jpg:Metal Man,audio/C:/Users/HP/OneDrive/SAND/game/assets/Mario Party 2 - Intro (720p_30fps_H264-192kbit_AAC).mp4:Mario Party 2
-video/C:/Users/HP/OneDrive/SAND/game/assets/The friend thats always waiting on you to make a mistake. CalebCity (720p_30fps_H264-192kbit_AAC).mp4:The friend thats always waiting on you to make a mistake.,What kind of healthcare was done in pre-colonial kenya?:Brain surgery,audio/C:/Users/HP/OneDrive/SAND/game/assets/Boku Dake Ga Inai Machi Opening (1080p_24fps_H264-128kbit_AAC).mp4:Erased,"img/C:/Users/HP/OneDrive/SAND/game/assets/MV5BYjY2MGM3NTgtMWI0MC00YTk3LTlmYjgtMzY4ODVhMjI3MDY1XkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_QL75_UX500_CR0,47,500,281_-3613079497.jpg:Rise of the black bat",audio/C:/Users/HP/OneDrive/SAND/game/assets/Diablo 2_ Resurrected - Mephisto Boss Fight 4K (2160p_60fps_VP9-160kbit_Opus).webm:Diablo 2
-,"img/C:/Users/HP/OneDrive/SAND/game/assets/Swaziland_flag_map-1394117338.webp:Johannesburg, Pretoria, Mbabane",audio/C:/Users/HP/OneDrive/SAND/game/assets/Psycho-Pass Opening 1 (Creditless) (1080p_24fps_H264-128kbit_AAC).mp4:Psycho-Pass Opening 1,img/C:/Users/HP/OneDrive/SAND/game/assets/MV5BMTcwNjI5MTAzNF5BMlf5BanBnXkFtZTcwNTcyOTIwNQ@@._V1_FMjpg_UX1000_-1286440561.jpg:Almighty Thor,img/C:/Users/HP/OneDrive/SAND/game/assets/hmmm.JPG:Dead Island`
+var QUESTIONS_ANSWERS = '';
+
+// load csv file into mem
+const client = new XMLHttpRequest();
+client.open('GET', '/assets/game.csv');
+client.onreadystatechange = function() {
+   QUESTIONS_ANSWERS = client.responseText;
+}
+client.send();
+
 
 class TriviaGameShow {
    constructor(element) {
@@ -38,6 +39,10 @@ class TriviaGameShow {
       //Bind event handlers
       this.boardElement.addEventListener("click", event => {
          this.handleClueClick(event);
+      });
+
+      this.modalElement.addEventListener("click", event => {
+         this.handleFormSubmit(event);
       });
       
       //Render initial state of score
@@ -103,28 +108,31 @@ class TriviaGameShow {
       if (isFile(question)) {
          // make html for video and image (do extension endsWith to check)
          let filePath = '';
-
+         let relativePath = '';
          if (question.startsWith('img/'))
          {
+            relativePath = question.slice(35, question.length)
             filePath = question.split('img/')[1];
-            newQuestion = `<img src="${filePath}" style="max-width:100%;max-height:100%;"/>`
+            newQuestion = `<img src="${relativePath}" style="max-width:100%;max-height:100%;"/>`
          }
          else if (question.startsWith('video/')) {
+            relativePath = question.slice(37, question.length)
             filePath = question.split('video/')[1];
-            newQuestion = `<video controls>
-            <source src="${filePath}" type="video/mp4">
+            newQuestion = `<video controls style="max-width:100%;max-height:100%;">
+            <source src="${relativePath}" type="video/mp4">
             Your browser does not support the audio element.
          </video>`
          }
          else if (question.startsWith('audio/')) {
-            filePath = clue.split('audio/')[1];
+            relativePath = question.slice(37, question.length)
+            filePath = question.split('audio/')[1];
             newQuestion = `<audio controls>
-            <source src="${filePath}" type="audio/mpeg">
+            <source src="${relativePath}" type="audio/mpeg">
             Your browser does not support the audio element.
          </audio>`
          }
 
-         newQuestion = newQuestion.slice(35, newQuestion.length)
+         
       }
       else {
          // text question
@@ -179,10 +187,10 @@ class TriviaGameShow {
       event.preventDefault();
       
       //Show answer
-      this.revealAnswer(isCorrect);
+      this.revealAnswer();
    }
 
-   revealAnswer(isCorrect) { 
+   revealAnswer() { 
       //Show the whole result container
       this.modalElement.classList.add("showing-result");
       
@@ -190,7 +198,7 @@ class TriviaGameShow {
       // TODO: make x btn
       setTimeout(() => {
          this.modalElement.classList.remove("visible");
-      }, 3000);
+      }, 10000);
    }
    
 
